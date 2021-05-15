0da48dead987bc1b3a4e725bc47b6111f7163046
gpsiegel <gpsiegel@hotmail.com>
2021-05-15 17:53:16 -0500

    adding subnets and igs

--- vpc_peer.tf
+++ vpc_peer.tf
@@ -7,10 +7,62 @@ resource "aws_vpc" "us-east" {
   }
 }
 
+#IG 1
+resource "aws_internet_gateway" "gw-east" {
+  vpc_id = aws_vpc.us-east.id
+  tags = {
+      Name = "VPC-1"
+  }
+}
+
+#Subnets 1-1
+resource "aws_subnet" "main-east" {
+  vpc_id     = aws_vpc.us-east.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+      Name = "VPC-1"
+  }
+
+}
+
+#Subnets 1-2
+resource "aws_subnet" "secondary-east" {
+  vpc_id     = aws_vpc.us-east.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+      Name = "VPC-1"
+  }
+
+}
+
 #VPC 2
 resource "aws_vpc" "us-west" {
   provider = aws.region-2  
   cidr_block = "172.16.0.0/16"
+  tags = {
+      Name = "VPC-2"
+  }
+}
+
+#IG 2
+resource "aws_internet_gateway" "gw-west" {
+  vpc_id = aws_vpc.us-west.id
+}
+
+#Subnet 2-1
+resource "aws_subnet" "main-west" {
+  vpc_id     = aws_vpc.us-west.id
+  cidr_block = "172.16.1.0/24"
+  tags = {
+      Name = "VPC-2"
+  }
+
+}
+
+#Subnet 2-2
+resource "aws_subnet" "secondary-west" {
+  vpc_id     = aws_vpc.us-west.id
+  cidr_block = "172.16.2.0/24"
   tags = {
       Name = "VPC-2"
   }

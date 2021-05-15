0d77b9cc4939354026153c172383bba3c5580984
gpsiegel <gpsiegel@hotmail.com>
2021-05-15 16:52:14 -0500

    edited vpc_peer.tf

--- vpc_peer.tf
+++ vpc_peer.tf
@@ -16,11 +16,13 @@ resource "aws_vpc" "us-west" {
   }
 }
 
+data "aws_caller_identity" "current" {}
 
 #Peering
 resource "aws_vpc_peering_connection" "us-east-west-peer" {
   peer_vpc_id   = aws_vpc.us-west.id
   vpc_id        = aws_vpc.us-east.id
+  peer_owner_id = data.aws_caller_identity.current.account_id
   peer_region   = "us-west-1"
   auto_accept = false
 
@@ -29,7 +31,7 @@ resource "aws_vpc_peering_connection" "us-east-west-peer" {
   }
 }
 
-resource "aws_vpc_peering_connection_accepter" "eastwest-accept-peer" {
+resource "aws_vpc_peering_connection_accepter" "peer" {
   provider                  = aws.region-2
   vpc_peering_connection_id = aws_vpc_peering_connection.us-east-west-peer.id
   auto_accept               = true

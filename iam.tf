b4ee93f187430ad2464a963b0b55d88a7fe757ff
gpsiegel <gpsiegel@hotmail.com>
2021-05-15 16:17:51 -0500

    fixed iam policy for vpc

--- iam.tf
+++ iam.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "pipeline-policies" {
     }
     statement{
         sid = ""
-        actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
+        actions = ["cloudwatch:*", "s3:*", "vpc:*", "codebuild:*"]
         resources = ["*"]
         effect = "Allow"
     }

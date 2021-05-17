7a046c90e24405cc3325fd88ab4b69af7218c505
gpsiegel <gpsiegel@hotmail.com>
2021-05-17 11:30:48 -0500

    editing vpc_peer

--- vpc_peer.tf
+++ vpc_peer.tf
@@ -2,6 +2,8 @@
 resource "aws_vpc" "us-east" {
   provider = aws.region-1
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
       Name = "VPC-1"
   }
@@ -15,10 +17,18 @@ resource "aws_internet_gateway" "gw-east" {
   }
 }
 
+#AZs
+data "aws_availability_zones" "azs-1" {
+  provider = aws.region-1
+  state    = "available"
+}
+
+
 #Subnets 1-1
 resource "aws_subnet" "main-east" {
   vpc_id     = aws_vpc.us-east.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = element(data.aws_availability_zones.azs-1.names, 0)
   tags = {
       Name = "VPC-1"
   }
@@ -29,6 +39,7 @@ resource "aws_subnet" "main-east" {
 resource "aws_subnet" "secondary-east" {
   vpc_id     = aws_vpc.us-east.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = element(data.aws_availability_zones.azs-1.names, 1)
   tags = {
       Name = "VPC-1"
   }
@@ -39,10 +50,13 @@ resource "aws_subnet" "secondary-east" {
 resource "aws_vpc" "us-west" {
   provider = aws.region-2  
   cidr_block = "172.16.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
       Name = "VPC-2"
   }
 }
+
 
 #IG 2
 resource "aws_internet_gateway" "gw-west" {
@@ -52,10 +66,17 @@ resource "aws_internet_gateway" "gw-west" {
   }
 }
 
+data "aws_availability_zones" "azs-2" {
+  provider = aws.region-2
+  state    = "available"
+}
+
+
 #Subnet 2-1
 resource "aws_subnet" "main-west" {
   vpc_id     = aws_vpc.us-west.id
   cidr_block = "172.16.1.0/24"
+  availability_zone = element(data.aws_availability_zones.azs-2.names, 0)
   tags = {
       Name = "VPC-2"
   }
@@ -66,6 +87,7 @@ resource "aws_subnet" "main-west" {
 resource "aws_subnet" "secondary-west" {
   vpc_id     = aws_vpc.us-west.id
   cidr_block = "172.16.2.0/24"
+  availability_zone = element(data.aws_availability_zones.azs-2.names, 1)
   tags = {
       Name = "VPC-2"
   }
@@ -94,4 +116,57 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
   tags = {
     Side = "Accepter"
   }
+}
+
+#us-east-1 Route Table
+resource "aws_route_table" "east-rt" {
+  provider = aws.region-1
+  vpc_id   = aws_vpc.us-east.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw-east.id
+  }
+  route {
+    cidr_block                = "172.16.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.us-east-west-peer.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "VPC1-RT"
+  }
+}
+
+resource "aws_main_route_table_association" "east-default-rt" {
+  provider       = aws.region-1
+  vpc_id         = aws_vpc.us-east.id
+  route_table_id = aws_route_table.east-rt.id
+}
+
+#us-west-1 Route Table
+resource "aws_route_table" "west-rt" {
+  provider = aws.region-2
+  vpc_id   = aws_vpc.us-west.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw-west.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.us-east-west-peer.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "VPC2-RT"
+  }
+}
+
+#default us-west-1 rt
+resource "aws_main_route_table_association" "west-default-rt" {
+  provider       = aws.region-2
+  vpc_id         = aws_vpc.us-west.id
+  route_table_id = aws_route_table.west-rt.id
 }
